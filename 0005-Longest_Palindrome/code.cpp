337d9bf722a5989a6ee1510ebe72d703fa726348
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-15 07:07:18 +0530

    Add approach and code of problem 5

--- 0005-Longest_Palindrome/code.cpp
+++ 0005-Longest_Palindrome/code.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
 using namespace std;
 
-class Solution
-{
-    public:
-        vector<int> twoSum(vector<int> &nums, int target)
-        {
-            unordered_map<int, int> m;
-            for(int i = 0; i<nums.size(); i++)
-            {
-                int complement = target - nums[i];
-                if(m.find(complement) != m.end())
-                {
-                    return {m[complement], i};
-                }
-                else
-                {
-                    m[nums[i]] = i;
-                }
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int resstart=0, reslen=1;
+        if(s.size()==1)return s;
+        int start=0;
+        for(int i=0;i<s.size();i++){
+            start=i;
+            while(i+1<s.size()&& s[i]==s[i+1])
+                i++;
+            int end=i+1;
+            start--;
+            while(start>=0 && end<s.size() && s[start]==s[end]){
+                start--;
+                end++;
             }
-            return {};
+            start++;
+            end--;
+            if(reslen<end-start+1){
+                reslen=end-start+1;
+                resstart=start;
+            }
         }
+        return s.substr(resstart, reslen);
+    }
 };
